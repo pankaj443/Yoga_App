f156c909cb5d7716a02e576cae64ecdf1cdffb96
pankaj443 <pankajofficial443@gmail.com>
2019-04-21 02:01:05 +0530

    fragments added

--- app/src/main/java/com/py/yoga/activities/MainActivity.java
+++ app/src/main/java/com/py/yoga/activities/MainActivity.java
@@ -1,42 +1,57 @@
-package com.py.yoga;
+package com.py.yoga.activities;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import com.py.yoga.R;
+import com.py.yoga.fragments.tipsFragment;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment fragment = new tipsFragment();
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+                case R.id.navigation_workout:
+
+
+                case R.id.navigation_excercises:
+
+
+                case R.id.navigation_tips:
+                    fragment = new tipsFragment();
+
+
+                case R.id.navigation_utilities:
+
+
             }
-            return false;
+            displayfrag(fragment);
+            return true;
         }
     };
+
+    public void displayfrag(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.contentmain, fragment)
+                .commit();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
